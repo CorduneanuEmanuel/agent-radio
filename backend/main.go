7a39af1abd9bb4812362690ecaa70af2a4d37b8c
blindseer1 <vladtiru05@gmail.com>
2026-05-06 09:21:43 +0300

    Add upload and sse endpoints

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Go server is running")
+	})
+
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method needs to be POST", http.StatusMethodNotAllowed)
+			return
+		}
+
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, "could not read file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		dst, err := os.Create("/music/" + header.Filename)
+		if err != nil {
+			http.Error(w, "could not save file", http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+		io.Copy(dst, file)
+
+		fmt.Fprintln(w, "upload successful")
+	})
+
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		fmt.Fprintf(w, "data: {\"status\": \"connected\"}\n\n")
+		w.(http.Flusher).Flush()
+
+		<-r.Context().Done()
 	})
 
 	fmt.Println("Server starting on :8080")
